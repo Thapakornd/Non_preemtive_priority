5b1d30b1f49d484b4f3f579babe3fe31ad776b12
Galez <gustheworld6@gmail.com>
2022-09-28 08:49:01 +0700

    Update Non_preem_prio.py

--- Non_preem_prio.py
+++ Non_preem_prio.py
@@ -1,9 +1,10 @@
+
 import time
 from alive_progress import alive_bar
 import pandas as pd  # <------ import pandas
 
 # Read file from excel and get value
-dic = pd.read_excel('test.xlsx')
+dic = pd.read_excel('test11.xlsx')
 proc = list(dic.values)
 totalprocess = len(proc)
 
@@ -21,6 +22,7 @@ def sort_proc():
 def sort_prio(b_t,p_t):
     # Check value while arrival time 
     stk = 0
+    arr_t = []
     for i in range(p_t,totalprocess):
         if proc[i][2] < b_t:
             stk += 1
@@ -32,51 +34,62 @@ def sort_prio(b_t,p_t):
                 proc[j] = proc[j+1]
                 proc[j+1] = temp
 
+    for i in range(p_t,stk,1):
+        arr_t.append(proc[i])
+
+    return arr_t
+
+def progress_bar(s_t,val,x):
+    print(f"\t\t{s_t[x]} -------- {s_t[x+1]}")
+    with alive_bar(s_t[x+1],title=f"{val}") as bar:
+        for i in range(s_t[x+1]):
+            time.sleep(0.001)
+            bar()
+
 def find_va():
     s_time = [] # <------ Comput time each process
     tr_t = [0] * totalprocess   # <------ Turn around time each process
     wt = [0] * totalprocess   # <------ Waiting time each process
-    s_time.append(0)  # <------ Set P1 start 0 
+        # <------ Set P1 start 0 
     p_t = 0  # <---- Set Process computed
     sum_wt = 0
     sum_tr = 0
-
+    
     # Function sort arrival time    
     sort_proc()
 
-    cpu_un = 0
-    for i in range(totalprocess):
-        cpu_un += proc[i][1]
     i = 0
-    x = 0
+    first_1 = 0
     while True:
-        if proc[x][2] == i:
+        if first_1 == 0:
             s_time.append(i)
-            s_time.append(i+proc[x][1])
-            if proc[x][1] == proc[-1][1]:
-                break
-            p_t += 1
-            sort_prio(s_time[x],p_t)
-            x += 1
+            if proc[0][2] == 0:
+                s_time.append(i+proc[0][1])
+                p_t += 1
+                progress_bar(s_time,proc[0][0],0)
+                temp_re = sort_prio(i+proc[0][1],p_t)
+            else:
+                s_time.append(proc[0][2])
+                p_t += 1
+                progress_bar(s_time,"",0)
+                s_time.append(s_time[1] + proc[0][1])
+                progress_bar(s_time,proc[0][0],1)
+                temp_re = sort_prio(s_time[1] + proc[0][1],p_t)
+            first_1 += 1
+        elif temp_re[0][2] == i:
+            if s_time[-1] == i:
+                s_time
+            else:
+                pass      
         i += 1
+        
 
-    # CPU untilization
-    cpu_un = (cpu_un / s_time[-1]) * 100 
-
-    # Output
-    '''for i in range(totalprocess):
-        print(f"\t\t{s_time[i]} ------- {s_time[i+1]}")
-        with alive_bar(proc[i][1],title=f"{proc[i][0]}") as bar:
-            for x in range(proc[i][1]):
-                time.sleep(0.001)
-                bar()
-    '''
     print("\nProcess \tBursttime \tArrivaltime \tPriority \tWaiting \tTurnaround")
     for i in range(totalprocess):
         print("{} \t\t{} \t\t{} \t\t{} \t\t{} \t\t{}".format(proc[i][0],proc[i][1],proc[i][2],proc[i][3],wt[i],tr_t[i]))
-    #print("\n\nAvg waiting time : {:.2f}".format(sum_wt))
-    #print("Avg turnaround time : {:.2f}".format(sum_tr))
-    #print("Throughput : {:.2f} process/sec\n".format(totalprocess / (s_time[-1])))
+    print("\n\nAvg waiting time : {:.2f}".format(sum_wt))
+    print("Avg turnaround time : {:.2f}".format(sum_tr))
+    print("Throughput : {:.2f} process/sec\n".format(totalprocess / (s_time[-1])))
 
 
 # Calculate queue non-preem-prioriy
